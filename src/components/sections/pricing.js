6c3c8b3c3b442e7d2f96c12df0e79972e696f2e7
NikhilNamal17 <nikhilnamal16@gmail.com>
2020-01-27 16:53:24 +0530

    fix pricing button

--- src/components/sections/pricing.js
+++ src/components/sections/pricing.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { PricingTable, PricingSlot, PricingDetail } from 'react-pricing-table';
 import styled from "styled-components"
-
 import { Section, Container } from "../global"
-// import { ActionsContainer } from "../common/navigation/style"
-// import AnchorLink from "react-anchor-link-smooth-scroll"
+import "../../styles/pricingButton.css"
+
 
 class Pricing extends React.Component {
     handleSubmit = event => {
@@ -21,26 +20,19 @@ class Pricing extends React.Component {
                     <SectionTitle>Find a plan that's right for you.</SectionTitle>
                     <FeatureText>Every  Plan Includes 15-Day Free Trial.</FeatureText>
                     <PricingTable highlightColor='#098c8c'>
-                        <PricingSlot id="pricing" onClick={this.handleSubmit} buttonText="SIGN UP" title='FREE' priceText='&#8377; 0'>
+                        <PricingSlot buttonClass="pricingButton" id="pricing" onClick={this.handleSubmit} buttonText="SIGN UP" title='FREE' priceText='&#8377; 0'>
                             <PricingDetail> <b>1 Screen</b></PricingDetail>
                             <PricingDetail> <b>Video/Display Ads</b> </PricingDetail>
                             <PricingDetail> <b>Limited Subjects</b> </PricingDetail>
                             <PricingDetail strikethrough> <b>Dark Mode</b></PricingDetail>
-
-                            {/* <ActionsContainer>
-                                <AnchorLink href="#top">
-                                    <ContainerButton>Sign up</ContainerButton>
-                                </AnchorLink>
-                            </ActionsContainer> */}
-
                         </PricingSlot>
-                        <PricingSlot id="pricing" highlighted onClick={this.handleSubmit} buttonText='SIGN UP' title='GOLD' priceText='&#8377; 400/semester'>
+                        <PricingSlot buttonClass="pricingButton" id="pricing" highlighted onClick={this.handleSubmit} buttonText='SIGN UP' title='GOLD' priceText='&#8377; 400/semester'>
                             <PricingDetail> <b>2 Screen</b></PricingDetail>
                             <PricingDetail> <b>All Subjects</b> </PricingDetail>
                             <PricingDetail> <b>Dark Mode</b></PricingDetail>
                             <PricingDetail strikethrough> <b>Video/Display Ads</b> </PricingDetail>
                         </PricingSlot>
-                        <PricingSlot id="pricing" onClick={this.handleSubmit} buttonText='SIGN UP' title='DIAMOND' priceText='&#8377; 700/year'>
+                        <PricingSlot buttonClass="pricingButton" id="pricing" onClick={this.handleSubmit} buttonText='SIGN UP' title='DIAMOND' priceText='&#8377; 700/year'>
                             <PricingDetail> <b>2 Screen</b></PricingDetail>
                             <PricingDetail> <b>All Subjects</b> </PricingDetail>
                             <PricingDetail> <b>Dark Mode</b></PricingDetail>
@@ -77,7 +69,4 @@ const FeatureText = styled.p`
   text-align: center;
 `
 
-// const ContainerButton = styled.button`
-//  text-align:center
-// `
 export default Pricing;
